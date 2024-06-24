b95f80e6e2757a3d7772103b84aeb6670843d352
Santosh <ksantosh@intelops.dev>
2024-06-24 23:55:11 +0530

    Remove trouble depreication message in telescope

--- santoshdts/plugins/telescope.lua
+++ santoshdts/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    "folke/trouble.nvim", -- ensure trouble.nvim is included as a dependency
   },
   config = function()
     local telescope = require("telescope")
@@ -30,7 +31,7 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-t>"] = require("trouble.sources.telescope").open, -- updated to use the new function
           },
         },
       },
@@ -46,6 +47,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fg", builtin.live_grep,{desc = "Grep strings"})
+    keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep strings" })
   end,
 }
