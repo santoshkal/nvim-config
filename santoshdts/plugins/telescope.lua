ed7c46af6253396a9675753ebafe311e44342c79
Santosh <ksantosh@intelops.dev>
2024-06-24 20:51:45 +0530

    Add grep to telescope

--- santoshdts/plugins/telescope.lua
+++ santoshdts/plugins/telescope.lua
@@ -12,6 +12,15 @@ return {
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
+    local trouble = require("trouble")
+    local trouble_telescope = require("trouble.providers.telescope")
+    local builtin = require('telescope.builtin')
+    -- or create your custom action
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function(prompt_bufnr)
+        trouble.toggle("quickfix")
+      end,
+    })
 
     telescope.setup({
       defaults = {
@@ -20,7 +29,8 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
           },
         },
       },
@@ -36,5 +46,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fg", builtin.live_grep,{desc = "Grep strings"})
   end,
 }
